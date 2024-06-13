6204cd3d4193493536cf61d61532bcadad5d9d9e
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-06-13 15:58:43 -0400

    Update App.tsx

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 
 function App() {
-  const [count, setCount] = React.useState(0);
+  interface User {
+    name: string;
+    age: number;
+  }
+
+  const user: User = {
+    name: "James",
+    age: 30,
+  };
+
   return (
     <div className="text-3xl font-bold">
       <h1>Hello World</h1>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(1)}>Add</button>
+      <h1>My name is {user.name}</h1>
+      <h1>My age is {user.age}</h1>
     </div>
   );
 }
