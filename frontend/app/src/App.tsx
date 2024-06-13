43eeb0a367e09d86855e2de1a0c9b33faaa3e21a
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-06-13 11:19:42 -0400

    Update App.tsx

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,9 +1,12 @@
-// import React from "react";
+import React from "react";
 
 function App() {
+  const [count, setCount] = React.useState(0);
   return (
     <div className="text-3xl font-bold">
       <h1>Hello World</h1>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(1)}>Add</button>
     </div>
   );
 }
