bb417b726f14b42ec4b92e143c3fc936903b7282
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-06-26 17:51:05 -0400

    Update

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,17 +1,19 @@
-import React from "react";
 import NavBar from "./navbar/NavBar";
 import SuggestionsMenu from "./suggestions-menu/SuggestionsMenu";
 import { Outlet } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <div>
         <NavBar />
         <SuggestionsMenu />
         <Outlet />
       </div>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- frontend/app/src/feedback/Feedback.tsx
+++ frontend/app/src/feedback/Feedback.tsx
@@ -1,8 +1,4 @@
-import React from "react";
-import { data } from "../data/fakeData.js";
-
 function Feedback() {
-  console.log(data);
   return <div className="bg-gray-100">Feedback</div>;
 }
 
