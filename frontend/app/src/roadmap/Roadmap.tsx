803a3f844b66675ed0fa5ec2ac85a8206b2ea0e5
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-08-10 14:30:52 -0400

    Tab component completed

--- frontend/app/src/roadmap/Roadmap.tsx
+++ frontend/app/src/roadmap/Roadmap.tsx
@@ -5,24 +5,34 @@ import { fetchFeedbacks } from "../api/fetchFeedbacks";
 import RoadmapList from "./RoadmapList";
 
 function Roadmap() {
-  interface Data {
-    id: number;
-    title: string;
-    category: string;
-    upvotes: number;
-    status: string;
-    description: string;
-    comments: [];
-  }
   const { isPending, isError, data } = useQuery({
     queryKey: ["feedbacks"],
     queryFn: () => fetchFeedbacks("http://127.0.0.1:8000/product-requests/"),
     staleTime: 120000,
   });
-  const [RoadmapListData, setListData] = React.useState<Data[]>();
   const planned = data?.filter((item: any) => item.status === "planned");
   const inProgress = data?.filter((item): any => item.status === "in-progress");
   const live = data?.filter((item) => item.status === "live");
+  const [tabIndex, setTab] = React.useState(0);
+
+  const tabs = [
+    <div>
+      <h1 className="font-bold text-lg">Planned({planned?.length})</h1>
+      <p className="text-gray-500 mb-5">Ideas prioritized for research</p>
+      <RoadmapList data={planned} border={"border-[#F49F85]"} />
+    </div>,
+    <div>
+      <h1 className="font-bold text-lg">In-Progress({inProgress?.length})</h1>
+      <p className="text-gray-500 mb-5">Currently being developed</p>
+      <RoadmapList data={inProgress} border={"border-[#AD1FEA]"} />
+    </div>,
+    <div>
+      <h1 className="font-bold text-lg">Live({live?.length})</h1>
+      <p className="text-gray-500 mb-5">Released Features</p>
+      <RoadmapList data={live} border={"border-[#62BCFA]"} />
+    </div>,
+  ];
+
   return (
     <div>
       <div className="flex items-center justify-between px-5 py-10 bg-[#373F68] text-white">
@@ -41,27 +51,20 @@ function Roadmap() {
           </button>
         </Link>
       </div>
+
       <div className="flex items-center justify-between border-b-2 text-gray-400 font-semibold bg-[#F7F8FD]">
-        <button
-          className="px-5 py-8 cursor-pointer"
-          onClick={() => setListData(planned)}
-        >
+        <button className="px-5 py-8 cursor-pointer" onClick={() => setTab(0)}>
           Planned({planned?.length})
         </button>
-        <button
-          className="px-5 py-8 cursor-pointer"
-          onClick={() => setListData(inProgress)}
-        >
+        <button className="px-5 py-8 cursor-pointer" onClick={() => setTab(1)}>
           In-Progress({inProgress?.length})
         </button>
-        <button
-          className="px-5 py-8 cursor-pointer"
-          onClick={() => setListData(live)}
-        >
+        <button className="px-5 py-8 cursor-pointer" onClick={() => setTab(2)}>
           Live({live?.length})
         </button>
       </div>
-      <RoadmapList data={RoadmapListData} />
+
+      <div className="p-5 bg-[#F7F8FD]">{tabs[tabIndex]}</div>
     </div>
   );
 }
--- frontend/app/src/roadmap/RoadmapList.tsx
+++ frontend/app/src/roadmap/RoadmapList.tsx
@@ -1,4 +1,4 @@
-function RoadmapList({ data }: any) {
+function RoadmapList({ data, border }: any) {
   interface Data {
     id: number;
     title: string;
@@ -9,39 +9,32 @@ function RoadmapList({ data }: any) {
     comments: [];
   }
   console.log(data);
+
   return (
     <>
-      <div className="bg-[#F7F8FD] p-5 space-y-5">
-        <div>
-          <h1 className="font-bold text-xl">
-            {data && data[0].status} ({data?.length})
-          </h1>
-          <p className="text-gray-500">Features currently being developed</p>
-        </div>
-        {data?.map((items: Data) => (
-          <div
-            className="bg-white p-8 rounded-lg space-y-3 border-t-8 border-[#AD1FEA]"
-            key={items.id}
-          >
-            <h1 className="font-bold hover:text-blue-500">{items.title}</h1>
-            <p className="text-gray-500">{items.description}</p>
-            <span className="bg-blue-100 text-blue-500 font-bold px-5 py-2 rounded-lg inline-block">
-              {items.category}
-            </span>
-            <div className="flex justify-between items-center">
-              <button className="bg-blue-100 font-bold px-5 py-2 rounded-lg flex items-center gap-2 hover:bg-purple-300">
-                <img src="/public/assets/shared/icon-arrow-up.svg" />
-                {items.upvotes}
-              </button>
+      {data?.map((items: Data) => (
+        <div
+          className={`bg-white p-8 rounded-lg space-y-3 border-t-8 mb-5 ${border}`}
+          key={items.id}
+        >
+          <h1 className="font-bold hover:text-blue-500">{items.title}</h1>
+          <p className="text-gray-500">{items.description}</p>
+          <span className="bg-blue-100 text-blue-500 font-bold px-5 py-2 rounded-lg inline-block">
+            {items.category}
+          </span>
+          <div className="flex justify-between items-center">
+            <button className="bg-blue-100 font-bold px-5 py-2 rounded-lg flex items-center gap-2 hover:bg-purple-300">
+              <img src="/public/assets/shared/icon-arrow-up.svg" />
+              {items.upvotes}
+            </button>
 
-              <div className="flex items-center gap-2">
-                <img src="/public/assets/shared/icon-comments.svg" />
-                <p className="font-bold text-lg">{items.comments.length}</p>
-              </div>
+            <div className="flex items-center gap-2">
+              <img src="/public/assets/shared/icon-comments.svg" />
+              <p className="font-bold text-lg">{items.comments.length}</p>
             </div>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </>
   );
 }
