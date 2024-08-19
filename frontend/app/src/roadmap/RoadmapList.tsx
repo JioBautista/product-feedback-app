6ea9e9c2aadf6a7e9379750f839cc0caedc73474
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-08-19 17:45:34 -0400

    Updates

--- frontend/app/src/roadmap/RoadmapList.tsx
+++ frontend/app/src/roadmap/RoadmapList.tsx
@@ -14,16 +14,16 @@ function RoadmapList({ data, border }: any) {
     <>
       {data?.map((items: Data) => (
         <div
-          className={`bg-white p-8 rounded-lg border-t-8 mb-5 ${border} md:p-4 flex flex-col items-start justify-between gap-4 lg:p-6`}
+          className={`bg-white p-6 rounded-lg border-t-8 mb-5 ${border} md:p-4 flex flex-col items-start justify-between gap-2 lg:p-6`}
           key={items.id}
         >
           <h1 className="font-semibold hover:text-blue-500">{items.title}</h1>
           <p className="text-gray-500">{items.description}</p>
-          <span className="bg-blue-100 text-blue-500 font-semibold px-5 py-2 rounded-lg inline-block">
+          <span className="bg-blue-100 text-blue-500 font-semibold px-5 py-2 rounded-xl inline-block">
             {items.category}
           </span>
           <div className="flex justify-between items-center w-full">
-            <button className="bg-blue-100 font-semibold px-5 py-2 rounded-lg flex items-center gap-2 hover:bg-purple-300">
+            <button className="bg-blue-100 font-semibold px-5 py-2 rounded-xl flex items-center gap-2 hover:bg-purple-300">
               <img src="/public/assets/shared/icon-arrow-up.svg" />
               {items.upvotes}
             </button>
