e1c2db419e985c0781891546be6b0fd682c0a86d
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-08-19 17:13:57 -0400

    Update App.tsx

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -5,6 +5,7 @@ import { Outlet } from "react-router-dom";
 function App() {
   return (
     <div className="lg:grid lg:grid-cols-3 lg:container lg:mx-auto lg:p-10">
+      {/* Navigation bar component */}
       <NavBar />
       <SuggestionsMenu />
       <Outlet />
