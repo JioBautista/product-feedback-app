7fe190266df8686f7843c18cd0f6bb925b07bfb2
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-08-22 14:27:46 -0400

    Updates

--- frontend/app/src/main.tsx
+++ frontend/app/src/main.tsx
@@ -10,8 +10,20 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { fetchFeedbacks } from "./api/fetchFeedbacks.tsx";
 
 const queryClient = new QueryClient();
+
+try {
+  const data = await queryClient.fetchQuery({
+    queryKey: ["feedbacks"],
+    queryFn: () => fetchFeedbacks("http://127.0.0.1:8000/product-requests/"),
+    staleTime: 10000,
+  });
+  console.log(data);
+} catch (error) {
+  console.log(error);
+}
 
 const router = createBrowserRouter([
   {
