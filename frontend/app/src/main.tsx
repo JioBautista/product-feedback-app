bcaf587d0904d7814673eb40f96a276b4e2ac191
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-08-30 12:00:47 -0400

    Updates

--- frontend/app/src/main.tsx
+++ frontend/app/src/main.tsx
@@ -10,15 +10,8 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { fetchFeedbacks } from "./api/fetchFeedbacks.tsx";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      queryFn: () => fetchFeedbacks("http://127.0.0.1:8000/product-requests/"),
-    },
-  },
-});
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
--- frontend/app/src/suggestions-menu/SuggestionsMenu.tsx
+++ frontend/app/src/suggestions-menu/SuggestionsMenu.tsx
@@ -5,7 +5,9 @@ function SuggestionsMenu() {
   return (
     <div className="bg-[#373F68] text-white md:bg-[#F7F8FD] lg:col-span-2 md:px-5">
       <div className="container mx-auto px-5 py-3 flex justify-between items-center md:bg-[#373F68] md:rounded-xl">
+        {/* A dropdown component that sorts data by the number of upvotes or comments */}
         <SuggestionDropDown />
+
         <Link to={"/new"}>
           <button className="bg-[#AD1FEA] px-5 py-4 rounded-xl font-semibold tracking-wide lg:py-3 text-sm hover:bg-[#C75AF6]">
             + Add Feedback
