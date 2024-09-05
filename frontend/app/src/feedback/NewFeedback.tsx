1251e2227be419e43afad257fec68b1f51b871f9
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-09-05 13:40:50 -0400

    Form submitting started

--- frontend/app/src/feedback/NewFeedback.tsx
+++ frontend/app/src/feedback/NewFeedback.tsx
@@ -1,5 +1,35 @@
 import { Link } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+
 function NewFeedback() {
+  interface NewFeedback {
+    title: string;
+    category: string;
+    description: string;
+  }
+  const mutation = useMutation({
+    mutationFn: (data) => {
+      axios
+        .post("http://127.0.0.1:8000/product-requests/", data)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  });
+
+  const { register, handleSubmit } = useForm<NewFeedback>();
+  const onSubmit: SubmitHandler<NewFeedback> = (data) => {
+    const defaultData = { upvotes: 0, status: "suggestion" };
+    const newFeedback = { ...data, ...defaultData };
+
+    mutation.mutate(newFeedback);
+    console.log(newFeedback);
+  };
   return (
     <div className="px-5 py-10 space-y-10 max-w-[540px] mx-auto">
       <div className="flex items-center gap-5">
@@ -9,60 +39,62 @@ function NewFeedback() {
         </Link>
       </div>
 
-      <div className="bg-white p-10 rounded-lg space-y-5">
-        <h1 className="font-bold text-2xl">Create New Feedback</h1>
-        <div>
-          <h2 className="font-bold mb-1">Feedback Title</h2>
-          <label className="mb-5 text-gray-500 block" htmlFor="title">
-            Add a short, descriptive headline
-          </label>
-          <input
-            className="bg-[#F7F8FD] p-5 cursor-pointer text-gray-800 w-full rounded-lg focus:bg-gray-200"
-            id="title"
-            name="title"
-          />
-        </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="bg-white p-10 rounded-lg space-y-5">
+          <h1 className="font-bold text-2xl">Create New Feedback</h1>
+          <div>
+            <h2 className="font-bold mb-1">Feedback Title</h2>
+            <label className="mb-5 text-gray-500 block" htmlFor="title">
+              Add a short, descriptive headline
+            </label>
+            <input
+              className="bg-[#F7F8FD] p-5 cursor-pointer text-gray-800 w-full rounded-lg focus:bg-gray-200"
+              id="title"
+              {...register("title")}
+            />
+          </div>
 
-        <div>
-          <h2 className="font-bold mb-1">Category</h2>
-          <label className="mb-5 text-gray-500 block" htmlFor="category">
-            Choose a category for your feedback
-          </label>
-          <select
-            name="category"
-            id="category"
-            className="bg-[#F7F8FD] p-5 cursor-pointer text-gray-800 w-full rounded-lg border-r-8 border-transparent"
-          >
-            <option value={"Feature"}>Feature</option>
-            <option value={"UI"}>UI</option>
-            <option value={"UX"}>UX</option>
-            <option value={"Enhancement"}>Enhancement</option>
-            <option value={"Bug"}>Bug</option>
-          </select>
-        </div>
+          <div>
+            <h2 className="font-bold mb-1">Category</h2>
+            <label className="mb-5 text-gray-500 block" htmlFor="category">
+              Choose a category for your feedback
+            </label>
+            <select
+              className="bg-[#F7F8FD] p-5 cursor-pointer text-gray-800 w-full rounded-lg border-r-8 border-transparent"
+              id="category"
+              {...register("category")}
+            >
+              <option value={"Feature"}>Feature</option>
+              <option value={"UI"}>UI</option>
+              <option value={"UX"}>UX</option>
+              <option value={"Enhancement"}>Enhancement</option>
+              <option value={"Bug"}>Bug</option>
+            </select>
+          </div>
 
-        <div>
-          <h2 className="font-bold mb-1">Feedback Detail</h2>
-          <label className="mb-5 text-gray-500 block" htmlFor="details">
-            Include any specific comments on what should be improved, added,
-            etc.
-          </label>
-          <textarea
-            className="bg-[#F7F8FD] p-5 cursor-pointer text-gray-800 w-full rounded-lg focus:bg-gray-200"
-            name="details"
-            id="details"
-          ></textarea>
-        </div>
+          <div>
+            <h2 className="font-bold mb-1">Feedback Detail</h2>
+            <label className="mb-5 text-gray-500 block" htmlFor="details">
+              Include any specific comments on what should be improved, added,
+              etc.
+            </label>
+            <textarea
+              className="bg-[#F7F8FD] p-5 cursor-pointer text-gray-800 w-full rounded-lg focus:bg-gray-200"
+              id="details"
+              {...register("description")}
+            ></textarea>
+          </div>
 
-        <div className="space-y-5 md:flex md:flex-row-reverse md:items-center md:space-y-0 md:gap-5">
-          <button className="bg-blue-500 px-4 py-3 rounded-lg font-semibold tracking-wide text-white w-full md:w-auto text-sm hover:bg-[#C75AF6]">
-            Add Feedback
-          </button>
-          <button className="bg-blue-950 px-4 py-3 rounded-lg font-semibold tracking-wide text-white w-full md:w-auto text-sm hover:bg-[#656EA3]">
-            Cancel
-          </button>
+          <div className="space-y-5 md:flex md:flex-row-reverse md:items-center md:space-y-0 md:gap-5">
+            <button className="bg-blue-500 px-4 py-3 rounded-lg font-semibold tracking-wide text-white w-full md:w-auto text-sm hover:bg-[#C75AF6]">
+              Add Feedback
+            </button>
+            <button className="bg-blue-950 px-4 py-3 rounded-lg font-semibold tracking-wide text-white w-full md:w-auto text-sm hover:bg-[#656EA3]">
+              Cancel
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
