93132b1fdb191350bf4e6e10c991d48a4e78f855
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-09-14 16:24:17 -0400

    Updates

--- frontend/app/src/feedback/AddComment.tsx
+++ frontend/app/src/feedback/AddComment.tsx
@@ -18,20 +18,43 @@ function AddComment() {
         });
     },
   });
+
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    const currentUser = {
+      id: 1,
+      image: "./assets/user-images/image-zena.jpg",
+      name: "Zena Kelley",
+      username: "velvetround",
+    };
+    const commentData = { user: currentUser, ...data, replies: [] };
+    const commentField = { comments: [commentData] };
+    mutation.mutate(commentField);
+    console.log(commentField);
+  };
   return (
     <div className="bg-white p-5 rounded-lg lg:p-8">
-      <h1 className="text-lg font-semibold tracking-wide mb-5">Add Comment</h1>
-      <textarea
-        className="bg-[#F7F8FD] p-5 cursor-pointer text-gray-500 w-full rounded-lg mb-5"
-        placeholder="Type your comment here"
-        maxLength={250}
-      ></textarea>
-      <div className="flex items-center justify-between">
-        <p className="text-gray-500">250 characters left</p>
-        <button className="bg-blue-500 px-6 py-4 rounded-lg font-semibold tracking-wide text-white text-sm hover:bg-[#C75AF6]">
-          Post Comment
-        </button>
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1 className="text-lg font-semibold tracking-wide mb-5">
+          Add Comment
+        </h1>
+        <textarea
+          className="bg-[#F7F8FD] p-5 cursor-pointer text-gray-500 w-full rounded-lg mb-5"
+          placeholder="Type your comment here"
+          maxLength={250}
+          {...register("content")}
+        ></textarea>
+        <div className="flex items-center justify-between">
+          <p className="text-gray-500">250 characters left</p>
+          <button
+            className="bg-blue-500 px-6 py-4 rounded-lg font-semibold tracking-wide text-white text-sm hover:bg-[#C75AF6]"
+            type="submit"
+          >
+            Post Comment
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
